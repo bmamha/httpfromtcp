c57e082a25f860d264e0fe544ce1ed3e69188c0a
Bereket Amha <amha.bereket@gmail.com>
2025-04-13 02:15:14 +0300

    Refactorized code to use channelsfor reading file into 8 bit bytes

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"log"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -12,18 +13,38 @@ func main() {
 	if err != nil {
 		log.Fatal()
 	}
-
-	b := make([]byte, 8)
-	for {
-		n, err := file.Read(b)
-		if err != nil {
-			if err == io.EOF {
-				break
-			}
-			fmt.Printf("error: %s\n", err.Error())
-			break
-		}
-		str := string(b[:n])
-		fmt.Printf("read: %s\n", str)
+	stringsChannel := getLinesChannel(file)
+	for line := range stringsChannel {
+		fmt.Println("read: ", line)
 	}
+}
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	stringsChannel := make(chan string)
+	go func() {
+		defer f.Close()
+		defer close(stringsChannel)
+		content := ""
+		b := make([]byte, 8)
+		for {
+			n, err := f.Read(b)
+			if err != nil {
+				if err == io.EOF {
+					break
+				}
+				fmt.Printf("error: %s\n", err.Error())
+				return
+			}
+			str := string(b[:n])
+			parts := strings.Split(str, "\n")
+			for i := 0; i < len(parts)-1; i++ {
+
+				stringsChannel <- fmt.Sprintf("%s%s", content, parts[i])
+				content = ""
+			}
+			content += parts[len(parts)-1]
+		}
+	}()
+
+	return stringsChannel
 }
