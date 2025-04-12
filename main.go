4c361d8642ed2d4915b8f87fcf0d8a8e7275a439
Bereket Amha <amha.bereket@gmail.com>
2025-04-12 18:17:58 +0300

    Added 8 bytes reader to main

--- main.go
+++ main.go
@@ -2,8 +2,28 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"log"
+	"os"
 )
 
 func main() {
-	fmt.Println("I hope I get the job!")
+	file, err := os.Open("./messages.txt")
+	if err != nil {
+		log.Fatal()
+	}
+
+	b := make([]byte, 8)
+	for {
+		n, err := file.Read(b)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			fmt.Printf("error: %s\n", err.Error())
+			break
+		}
+		str := string(b[:n])
+		fmt.Printf("read: %s\n", str)
+	}
 }
